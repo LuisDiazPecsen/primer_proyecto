09fe7531b32492a2f97dc23a83a5f2dd69535376
LuisDiazPecsen <lucho03diazpecsen@gmail.com>
2022-03-08 00:13:02 -0500

    first commit

---
+++ Controller/ProductoController.php
@@ -0,0 +1,24 @@
+<?php
+
+class ProductoController
+{
+   public function __construct()
+   {
+   }
+
+   public function listar()
+   {
+   }
+
+   public function agregar()
+   {
+   }
+
+   public function editar()
+   {
+   }
+
+   public function eliminar()
+   {
+   }
+}
